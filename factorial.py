9b7f223ba021c45fbf93cd737cf79d30b813b9a9
KushajSolanki <kushajsolanki0007@gmail.com>
2022-11-29 20:37:05 +0530

    py

--- factorial.py
+++ factorial.py
@@ -1,7 +1,7 @@
-f = 1
-i = 1
-n = int(input("Enter a Number: "))
-while(i<=n):
-    f = f*i
-    i = i+1
-print("Factorial is ",f)
+n = int(input("Enter a number:"))
+#program for factorial of number
+f=1
+for i in range (1,n+1):
+    f=f*i
+    i+=1
+print("Factorial is :",f)
